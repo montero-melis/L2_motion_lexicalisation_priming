973146c546ca9669cdec28da0670bb67a66101ad
montero-melis <montero-melis@biling.su.se>
2016-12-13 16:51:09 +0100

    Annotated data to long format

--- processing/coding_manual-automatic.R
+++ processing/coding_manual-automatic.R
@@ -15,17 +15,17 @@ tr_compl <- read.csv("data/data_verbal_transcribed.csv", fileEncoding = "UTF-8",
 head(tr_compl)
 str(tr_compl)
 
-# remove training items (VideoTrial == 0)
-tr_compl <- tr_compl[tr_compl$VideoTrial != 0, ]
-# no recordings for participant 14
-tr_compl[tr_compl$Subject == "14", ]
 # keep track of original row number in the transcribed data file (+1 to match)
 tr_compl$rownb <- as.numeric(row.names(tr_compl)) + 1
+# remove training items (VideoTrial == 0)
+tr_compl <- tr_compl[tr_compl$VideoTrial != 0, ]
+# Note there are no recordings for participant 14
+tr_compl[tr_compl$Subject == "14", ]
+# remove that participant
+tr_compl <- tr_compl[tr_compl$Subject != "14", ]
 
-# drop some columns to have a df that's easier to work with, and remove all 
-# rows with empty transcriptions
-tr <- tr_compl %>% filter(Target != "")  %>% 
-  select(rownb, Subject:VideoName, Target)
+# drop some columns to have a df that's easier to work with
+tr <- tr_compl %>% select(rownb, Subject:VideoName, Target)
 
 head(tr)
 str(tr)
@@ -61,8 +61,12 @@ rm(target)
 # the data annotation is done by calling the script "annotation_regex.R"
 source("processing/annotation_regex.R", encoding = "UTF-8")
 
-tr[1:50,]
-tail(tr, 50)
+# Add a column that shows the number of matches per target description
+tr$NbMatches <- str_count(tr$Match, "%")
+# remove the initial "%" (for first matches); leave "%" as separator only:
+tr$Match <- str_replace(tr$Match, "^%", "")
+
+head(tr)
 
 
 #  ------------------------------------------------------------------------
@@ -88,21 +92,56 @@ tr_random$Comment_GMM <- ""
 
 # to file
 write.csv(tr_random, "processing/targets_random.csv", fileEncoding = "UTF-8")
+rm(tr_random)
 
 
 #  ------------------------------------------------------------------------
-#  The basic function
+#  Annotated data in long format (one row per match)
 #  ------------------------------------------------------------------------
-
-
 
 # The basic function takes a row from the data frame tr as input. It outputs
 # a new data frame with one row for each vocabulary match in the Target.
-# Each of the rows in the output df has the following columns:
+
+tolong <- function(df = tr) {
+  out <- data.frame()
+  # looping through each row is very slow, but it's transparent
+  for (i in 1:nrow(df)) {
+    if (df[i, "NbMatches"] > 0) {  # skip the row if no match
+      # each match as a value of vector
+      row_match <- unlist(str_split(df[i, "Match"], "%"))
+      # split into the string matched and the associated vocabulary entry
+      row_match_matrix <- str_split_fixed(row_match, ":", 2)
+      # combine with that row's info
+      curr_df <- data.frame(df[i, c("Subject", "Group", "Condition",
+                                    "VideoTrial", "VideoName")],
+                            row_match_matrix, row.names = NULL)
+      out <- rbind(out, curr_df)
+    }
+  }
+  names(out)[(length(out) - 1) : length(out)] <- c("Match", "VocabEntry")
+  out
+}
+
+# small example
+tr[33:39,]
+tolong(tr[33:39,])
+
+# do for the whole data set (slow ~ 15s)
+tr_long <- tolong()
+
+
+#  ------------------------------------------------------------------------
+#  Merge long data with all vocabulary info
+#  ------------------------------------------------------------------------
+
+# Each row in tr_long is merged with vocabulary to obtain the columns:
 
 # Subject: Subject identifier
+# Group: L1 / L2
+# Condition: priming condition (Path, Manner, Control)
+# VideoTrial: Order in which this event was seen
 # VideoName: scene (item) identifier
-# Target: The whole target description (for reference)
+# (Target: The whole target description (for reference))
 # Match: The specific match that was captured with the regex
 # VocabEntry: The corresponding vocabulary entry associated to the Match
 # Baseform: The base form (makes mainly sense for verbs)
@@ -110,45 +149,6 @@ write.csv(tr_random, "processing/targets_random.csv", fileEncoding = "UTF-8")
 # Meaning: The specific meaning expressed within the SemComp
 # SyntCateg: The broad syntactic category -- wil be mostly main verb or adjunct
 # SyntForm: The specific syntactic form
-
-annotate <- function(df_row, regex) {
-  str_detect(df_row$Target, regex)
-  str_subset(df_row$Target, regex)
-  str_extract(df_row$Target, regex)
-}
-
-annotate(tr[1, ], "baj.*o\\b")
-
-
-
-mymatch <- function(regex = NULL, df = tr){
-  df[with(df, str_detect(Target, regex)), ]
-}
-
-myextract <- function(regex = NULL, df = tr){
-  df[with(df, str_extract(Target, regex)), ]
-}
-
-
-mymatch("desde abajo")
-mymatch("para abajo")
-mymatch("(?<!desde )abajo")
-
-str_extract(tr$Target,"desde abajo")
-str_extract(tr$Target,"(?<!desde )abajo")
-str_extract(tr$Target,"(?<!(para)|(desde)|(hacia) )abajo")
-str_extract(tr$Target,"((para)|(desde)|(hacia)) abajo")
-
-
-
-mymatch("\\bsube\\b", tr[1:200,])
-
-
-
-#  ------------------------------------------------------------------------
-#  Identify and code main verbs in each target
-#  ------------------------------------------------------------------------
-
 
 
 
