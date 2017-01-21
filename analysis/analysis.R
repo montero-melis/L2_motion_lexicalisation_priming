8f2035081079e0dff9e56b6cd40654d616befb85
montero-melis <montero-melis@biling.su.se>
2017-01-21 17:20:12 +0100

    Adds analyses using mixed models

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -1,8 +1,9 @@
 ## Analyse data from participants in the primed conditions.
 
 library(dplyr)
+library(lazyeval)  # lazy evaluation used in bysubj() function [summarise_]
 library(ggplot2)
-library(lazyeval)
+library(lme4)
 
 #  ------------------------------------------------------------------------
 #  Load data and process
@@ -108,6 +109,170 @@ f_plot_prof(bysubj(d, "P_adj"), "Path in adjunct")
 f_plot_prof(bysubj(d, "M_anyw"), "Manner anywhere")
 f_plot_prof(bysubj(d, "M_V"), "Manner in V")
 f_plot_prof(bysubj(d, "M_adj"), "Manner in adjunct")
+
+
+
+#  ------------------------------------------------------------------------
+#  Logit mixed models and corresponding plots
+#  ------------------------------------------------------------------------
+
+
+# Create data subsets for the different comparisons -----------------------
+
+# Path vs control
+d_pc <- d %>% filter(Condition != "Manner")
+# contrast coding for group
+contrasts(d_pc$Group) <- - contr.sum(2)
+colnames(contrasts(d_pc$Group)) <- "L2_vs_NS"
+contrasts(d_pc$Group)
+# contrast coding for conditions
+d_pc$Condition <- factor(d_pc$Condition)
+contrasts(d_pc$Condition) <- contr.sum(2)
+colnames(contrasts(d_pc$Condition)) <- "P_vs_C"
+contrasts(d_pc$Condition)
+
+# Manner vs control
+d_mc <- d %>% filter(Condition != "Path")
+# contrast coding for group
+contrasts(d_mc$Group) <- - contr.sum(2)
+colnames(contrasts(d_mc$Group)) <- "L2_vs_NS"
+contrasts(d_mc$Group)
+# contrast coding for conditions
+d_mc$Condition <- factor(d_mc$Condition)
+contrasts(d_mc$Condition) <- - contr.sum(2)
+colnames(contrasts(d_mc$Condition)) <- "M_vs_C"
+contrasts(d_mc$Condition)
+
+
+# Path in the verb --------------------------------------------------------
+
+# plot all conditions for reference
+f_plot(bysubj(d, "P_V"), "Path in V")
+
+# Path-primed vs Control
+fm_pve_pc <- glmer(P_V ~ Group * Condition + (1 | Subject) + (1 | VideoName),
+                   data = d_pc, family = "binomial")
+summary(fm_pve_pc)
+
+# Manner-primed vs Control
+fm_pve_mc <- glmer(P_V ~ Group * Condition + (1 | Subject) + (1 | VideoName),
+                   data = d_mc, family = "binomial")
+summary(fm_pve_mc)
+
+
+# Path as adjunct ---------------------------------------------------------
+
+# plot all conditions for reference
+f_plot(bysubj(d, "P_adj"), "Path as adjunct")
+
+# Path-primed vs Control
+fm_pad_pc <- glmer(P_adj ~ Group * Condition + (1 | Subject) + (1 | VideoName),
+                   data = d_pc, family = "binomial")
+summary(fm_pad_pc)
+
+# Manner-primed vs Control
+fm_pad_mc <- glmer(P_adj ~ Group * Condition + (1 | Subject) + (1 | VideoName),
+                   data = d_mc, family = "binomial")
+summary(fm_pad_mc)
+
+
+# Manner in the verb ------------------------------------------------------
+
+# plot all conditions for reference
+f_plot(bysubj(d, "M_V"), "Manner in V")
+
+# Manner-primed vs Control
+fm_mve_mc <- glmer(M_V ~ Group * Condition + (1 | Subject) + (1 | VideoName),
+                                data = d_mc, family = "binomial")
+summary(fm_mve_mc)
+
+# Path-primed vs Control
+fm_mve_pc <- glmer(M_V ~ Group * Condition + (1 | Subject) + (1 | VideoName),
+                   data = d_pc, family = "binomial")
+summary(fm_mve_pc)
+
+
+# Manner as adjunct -------------------------------------------------------
+
+# plot all conditions for reference
+f_plot(bysubj(d, "M_adj"), "Manner as adjunct")
+
+
+# Manner-primed vs Control
+fm_mad_mc <- glmer(M_adj ~ Group * Condition + (1 | Subject) + (1 | VideoName),
+                   data = d_mc, family = "binomial")
+summary(fm_mad_mc)
+
+# Path-primed vs Control
+fm_mad_pc <- glmer(M_adj ~ Group * Condition + (1 | Subject) + (1 | VideoName),
+                   data = d_pc, family = "binomial")
+summary(fm_mad_pc)
+
+
+
+#  ------------------------------------------------------------------------
+#  Logit mixed models for proficiency effect (cloze scores)
+#  ------------------------------------------------------------------------
+
+# subset L2 data
+d_l2 <- d %>% filter(Group == "L2")
+# center ClozeScore
+d_l2$cCloze <- scale(d_l2$ClozeScore)
+# Code the Condition factor so that Control is the reference group
+d_l2$Condition <- factor(d_l2$Condition, levels = c("Control", "Path", "Manner"))
+colnames(contrasts(d_l2$Condition)) <- c("P_vs_C", "M_vs_C")
+contrasts(d_l2$Condition)
+
+
+# Path expression ---------------------------------------------------------
+
+# Path in Verb
+f_plot_prof(bysubj(d, "P_V"), "Path in V") + geom_text(aes(label = Subject))
+fm_pve_prof <- glmer(P_V ~ Condition * cCloze + (1 | Subject) + (1 | VideoName),
+                     data = d_l2, family = "binomial")
+summary(fm_pve_prof)
+# likelihood ratio test
+fm_pve_prof_null <- glmer(P_V ~ Condition + (1 | Subject) + (1 | VideoName),
+                     data = d_l2, family = "binomial")
+summary(fm_pve_prof_null)
+anova(fm_pve_prof_null, fm_pve_prof)
+
+# Path in adjunct
+f_plot_prof(bysubj(d, "P_adj"), "Path in adjunct")
+fm_pad_prof <- glmer(P_adj ~ Condition * cCloze + (1 | Subject) + (1 | VideoName),
+                     data = d_l2, family = "binomial")
+summary(fm_pad_prof)
+# likelihood ratio test
+fm_pad_prof_null <- glmer(P_adj ~ Condition + (1 | Subject) + (1 | VideoName),
+                     data = d_l2, family = "binomial")
+anova(fm_pad_prof, fm_pad_prof_null)
+
+# Path anywhere
+f_plot_prof(bysubj(d, "P_anyw"), "Path anywhere")
+fm_pan_prof <- glmer(P_anyw ~ Condition * cCloze + (1 | Subject) + (1 | VideoName),
+                     data = d_l2, family = "binomial")
+summary(fm_pan_prof)
+
+
+# Manner expression ---------------------------------------------------------
+
+# Manner in Verb
+f_plot_prof(bysubj(d, "M_V"), "Manner in V")
+fm_mve_prof <- glmer(M_V ~ Condition * cCloze + (1 | Subject) + (1 | VideoName),
+                     data = d_l2, family = "binomial")
+summary(fm_mve_prof)
+
+# Manner in adjunct
+f_plot_prof(bysubj(d, "M_adj"), "Manner in adjunct")
+fm_mad_prof <- glmer(M_adj ~ Condition * cCloze + (1 | Subject) + (1 | VideoName),
+                     data = d_l2, family = "binomial")
+summary(fm_mad_prof)
+
+# Manner anywhere
+f_plot_prof(bysubj(d, "M_anyw"), "Manner anywhere")
+fm_man_prof <- glmer(M_anyw ~ Condition * cCloze + (1 | Subject) + (1 | VideoName),
+                     data = d_l2, family = "binomial")
+summary(fm_man_prof)
 
 
 
