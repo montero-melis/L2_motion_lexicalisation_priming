e4f4ef13fcaad03111eb484f68b5b43d8de416e2
montero-melis <montero-melis@biling.su.se>
2017-02-28 15:55:57 +0100

    Went through translation task
    few corrections and some further analyses

--- analysis/translation_task.R
+++ analysis/translation_task.R
@@ -8,6 +8,9 @@
 
 library(dplyr)
 library(ggplot2)
+library(lme4)
+library(effects)
+library(lattice)
 
 
 #  ------------------------------------------------------------------------
@@ -57,7 +60,7 @@ score_transl("arrastrar", c("breda ut / skaka", "dra", "släpa"))
 
 find_unique(target_v = "bajar")
 score_transl("bajar", c("gå nedför", "gå ner från", "gå nerför", "komma nerför",
-                        "stiga nedför", "sänka", "ta ned", "ta ner"))
+                        "stiga ned för", "stiga nedför", "sänka", "ta ned", "ta ner"))
 
 find_unique(target_v = "cruzar")
 score_transl("cruzar", c("gå genom", "gå igenom", "gå tvärs över", "gå över", 
@@ -71,28 +74,37 @@ score_transl("entrar", c("gå in i", "gå in på", "göra entré", "in i",
                          "komma in i", "komma in på", "stiga in i"))
 
 find_unique(target_v = "rodar")
-score_transl("rodar", c("rulla", "snurra"))
+score_transl("rodar", c("hjula", "rulla", "snurra"))
 
 find_unique(target_v = "subir")
-score_transl("subir", c("bestiga", "gå upp", "gå upp i", "gå upp på", 
-                        "gå uppför", "höja", "klättra upp", "köra uppför",
-                        "ta sig upp", "ta sig uppför", "ta upp", "åka uppför"))
+score_transl("subir", c("bestiga", "gå upp", "gå upp i", "gå upp på", "gå uppför",
+                        "höja", "klättra upp", "köra uppför", "lyfta", "ta sig upp",
+                        "ta sig uppför", "ta upp", "åka uppför"))
 
 find_unique(target_v = "tirar de")
-score_transl("tirar de", c("dra", "dra från", "dra i"))
+score_transl("tirar de", c("dra", "dra från", "dra i", "släpa"))
 
 
 #  ------------------------------------------------------------------------
 #  Descriptives (summary tables)
 #  ------------------------------------------------------------------------
 
+# By condition and verb-type 
+transl %>% group_by(Condition, Type) %>%
+  summarise(Perc = round(sum(Score) / n(), 2))
+
 # By condition and verb
 transl %>% group_by(Condition, Type, Target_verb) %>%
   summarise(Perc = round(sum(Score) / n(), 2))
 
-# By condition and verb-type 
-transl %>% group_by(Condition, Type) %>%
+# By subject, condition and type
+subj_prop <- transl %>% group_by(Subject, Condition, Type) %>%
   summarise(Perc = round(sum(Score) / n(), 2))
+print(subj_prop[with(subj_prop, order(Perc, Subject)),], n = nrow(subj_prop))
+
+# By subject, condition, type and verb
+subj_byverb <- transl %>% group_by(Subject, Condition, Type, Target_verb) %>%
+  summarise(Correct = sum(Score))
 
 
 #  ------------------------------------------------------------------------
@@ -138,3 +150,34 @@ ggsave("analysis/figures/translation-task_lm.pdf", width = mywidth,
        height = myheight)
 ggsave("analysis/figures/translation-task_lm.png", width = mywidth,
        height = myheight)
+
+
+
+#  ------------------------------------------------------------------------
+#  Significance test (GLMM)
+#  ------------------------------------------------------------------------
+
+# It really makes most sense to focus on the control/baseline condition only
+d_fm_baseline <- transl[transl$Condition == "Control", ]
+# contrast coding for Type (Path vs Manner verb)
+contrasts(d_fm_baseline$Type) <- contr.sum(2)
+colnames(contrasts(d_fm_baseline$Type)) <- "MannerV_vs_PathV"
+contrasts(d_fm_baseline$Type)
+# center cloze scores
+d_fm_baseline$cClozeScore <- as.vector(scale(d_fm_baseline$ClozeScore, scale = FALSE))
+
+head(d_fm_baseline)
+
+# Model treats the individual verbs as random effects
+fm_transl <- glmer(Score ~ Type * cClozeScore + 
+                     (1 | Subject) + (1 | Target_verb),
+                   data = d_fm_baseline, family = "binomial")
+summary(fm_transl)
+
+# NB: the random effects are clearly not normally distributed, probably because
+# a) ceiling effects for subjects, b) verbs are not randomly sampled and some
+# are simply harder than others
+dotplot(ranef(fm_transl), postVar = TRUE)
+
+fm_transl_eff <- allEffects(fm_transl)
+plot(fm_transl_eff, type = "link")
