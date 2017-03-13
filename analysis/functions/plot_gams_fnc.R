2e560593af688e92b62f2918e43b3e63f0e62a17
monteromelis <montero.gui@gmail.com>
2017-03-13 23:04:06 +0100

    Changes gams to compare conditions within groups (NS, L2)

--- analysis/functions/plot_gams_fnc.R
+++ analysis/functions/plot_gams_fnc.R
@@ -3,7 +3,80 @@
 require(mgcv)
 require(itsadug)
 
+
+# function to plot the differences between Path/Manner and Baseline
+# There are two models to be plotted, one for NS, one for L2 speakers
+plot_gam_main <- function(fm, mygroup = NULL, 
+                          ylim1 = c(-4, 6), ylim2 = c(-.5, 6), ...) {
+  layout(matrix(1:4, ncol = 2, byrow=TRUE), heights = c(1.5, 1))
+  par(mai = c(.7, .8, .5, 0.2))
+  mycex <- 0.9
+  # plot Path-Verbs Baseline
+  plot_smooth(fm, view = 'Trial', cond = list(VbType_Cond = 'P_V.Baseline'),
+              col = 'blue', lty = 2, rug = FALSE, ylim = ylim1, rm.ranef=TRUE,
+              main = paste0(mygroup, ': Path verbs'), ylab = 'Log-likelihood\nof path-verb',
+              hide.label = TRUE)
+  # plot Path-Verbs path-primed
+  plot_smooth(fm, view = 'Trial', cond = list(VbType_Cond = 'P_V.Path'),
+              col = 'red', rug = FALSE, rm.ranef = TRUE, add = TRUE,
+              hide.label = TRUE)
+  # add legend
+  legend(x = 1, y = ylim1[2] + 0.75, legend = c('Path-exposed', 'Baseline'),
+         col = c('red', 'blue'), lty = 1:2, cex = mycex, box.lty = 0)
+  
+  # Manner-verbs Baseline
+  plot_smooth(fm, view = 'Trial', cond = list(VbType_Cond = 'M_V.Baseline'),
+              col = 'blue', lty = 2, rug = FALSE, ylim = ylim1, rm.ranef = TRUE,
+              main = paste0(mygroup, ': Manner verbs'), ylab = 'Log-likelihood\nof manner-verb',
+              hide.label = TRUE)
+  # Manner-verbs manner-primed
+  plot_smooth(fm, view = 'Trial', cond = list(VbType_Cond = 'M_V.Manner'),
+              col = 'red', rug = FALSE, rm.ranef = TRUE, add = TRUE,
+              hide.label = TRUE)
+  # add legend
+  legend(x = 1, y = ylim1[2] + 0.75, legend = c('Manner-exposed', 'Baseline'),
+         col = c('red', 'blue'), lty = 1:2, cex = mycex, box.lty = 0)
+  
+  # Now plot the estimated differences with itsadug::plot_diff()
+  plot_diff(fm, view = 'Trial', comp = list(VbType_Cond = c('P_V.Path', 'P_V.Baseline')),
+            ylim = ylim2, rm.ranef=TRUE, 
+            main = 'Path-exposed - Baseline',
+            ylab = 'Diff. in log-likelihood\nof path-verb', hide.label = TRUE,
+            ...)  # a hack I need to set the mark.diff argument as F, bc of some bug?
+  plot_diff(fm, view = 'Trial', comp = list(VbType_Cond = c('M_V.Manner', 'M_V.Baseline')),
+            ylim = ylim2, rm.ranef=TRUE, 
+            main = 'Manner-exposed - Baseline',
+            ylab = 'Diff. in log-likelihood\nof manner-verb', hide.label = TRUE)
+}
+
+
+# function to plot the effects by L2 speakers' proficiency from GAMs
+plot_L2_profic <- function(fm, primed_cond = NULL, ylim1 = c(-6, 8),
+                           cloze_range = c(10, 35)) {
+  cloze_scores <- seq(cloze_range[1], cloze_range[2], length.out = 6)
+  # we want to have one common legend for all plots
+  layout(rbind(1, matrix(2:7, ncol=3, byrow=TRUE)), heights = c(1, 6, 6))
+  # add common legend
+  par(mai=c(0,0,0,0))
+  plot.new()
+  legend(x = "center", ncol = 2, legend = c(paste0(primed_cond, '-exposed'), 'Baseline'),
+         col = c('red', 'blue'), lty = 1:2, box.lty = 0, cex = 1.5)
+  # Now make plot for the model estimates at the different cloze scores in cloze_scores
+  # par(mai=rep(0.2, 4))
+  par(mai = c(.7, .8, .5, 0))
+  for(cloze in cloze_scores) {
+    plot_smooth(fm, view = 'Trial', cond = list(Condition = 'Baseline', ClozeScore = cloze),
+                col = 'blue', lty = 2, rug = FALSE, ylim = ylim1, rm.ranef = TRUE,
+                hide.label = TRUE, ylab = paste0('Log-likelihood\nof ', primed_cond, " verb"),
+                main = paste('Cloze score =', cloze))
+    plot_smooth(fm, view = 'Trial', cond = list(Condition = primed_cond, ClozeScore = cloze),
+                col = 'red', rug = FALSE, rm.ranef = TRUE, hide.label = TRUE, add = TRUE)
+  }
+}
+
+
 # function to plot the differences between NS and L2 speakers from GAMs
+# NB: not used anymore if I'm modelling each group (NS, L2) separately
 plot_NS_L2 <- function(fm, primed_cond = NULL, ylim1 = c(-4, 6), ylim2 = c(-.5, 6), ...) {
   layout(matrix(1:4, ncol = 2, byrow=TRUE), heights = c(1.5, 1))
   par(mai = c(.7, .8, .5, 0.2))
@@ -48,27 +121,3 @@ plot_NS_L2 <- function(fm, primed_cond = NULL, ylim1 = c(-4, 6), ylim2 = c(-.5, 
             ylab = paste('Diff. in log-likelihood\nof ', primed_cond, 'verb'), hide.label = TRUE)
 }
 
-
-# function to plot the effects by L2 speakers' proficiency from GAMs
-plot_L2_profic <- function(fm, primed_cond = NULL, ylim1 = c(-6, 8),
-                           cloze_range = c(10, 35)) {
-  cloze_scores <- seq(cloze_range[1], cloze_range[2], length.out = 6)
-  # we want to have one common legend for all plots
-  layout(rbind(1, matrix(2:7, ncol=3, byrow=TRUE)), heights = c(1, 6, 6))
-  # add common legend
-  par(mai=c(0,0,0,0))
-  plot.new()
-  legend(x = "center", ncol = 2, legend = c(paste0(primed_cond, '-exposed'), 'Baseline'),
-         col = c('red', 'blue'), lty = 1:2, box.lty = 0, cex = 1.5)
-  # Now make plot for the model estimates at the different cloze scores in cloze_scores
-  # par(mai=rep(0.2, 4))
-  par(mai = c(.7, .8, .5, 0))
-  for(cloze in cloze_scores) {
-    plot_smooth(fm, view = 'Trial', cond = list(Condition = 'Baseline', ClozeScore = cloze),
-                col = 'blue', lty = 2, rug = FALSE, ylim = ylim1, rm.ranef = TRUE,
-                hide.label = TRUE, ylab = paste0('Log-likelihood\nof ', primed_cond, " verb"),
-                main = paste('Cloze score =', cloze))
-    plot_smooth(fm, view = 'Trial', cond = list(Condition = primed_cond, ClozeScore = cloze),
-                col = 'red', rug = FALSE, rm.ranef = TRUE, hide.label = TRUE, add = TRUE)
-  }
-}
