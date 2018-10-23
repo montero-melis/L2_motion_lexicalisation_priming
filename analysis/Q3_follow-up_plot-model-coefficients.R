11cbc2cd95f7db89571d46f44b4d288263086c7f
montero-melis <montero.gui@gmail.com>
2018-10-23 13:04:24 +0200

    Script and functions to extract subject estimates from GLMMs

--- analysis/Q3_follow-up_plot-model-coefficients.R
+++ analysis/Q3_follow-up_plot-model-coefficients.R
@@ -22,15 +22,17 @@
 # Notes:
 # - The likelihood measure mentioned above is explored in script "analysis/
 #   Q3_follow-up_compare-LL.R" -- results were difficult to interpret
-# - The exact idea presented above (a landscpae as TFJ describes) is not 
-#   feasible given the data, because VerbType and Condition are both between-
-#   subject variables
+# - The exact suggestion presented above (a landscpae as TFJ describes) is not
+#   feasible given the current design, because VerbType and Condition are
+#   both between-subject variables, so coef for Manner and Path cannot appear
+#   on the same plot.
+
 
 library("dplyr")
 library("lme4")
-# library("purrr")
 library("sjPlot")
 library("ggplot2")
+library("tidyr")
 
 
 
@@ -38,27 +40,40 @@ library("ggplot2")
 # Load data ---------------------------------------------------------------
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-## Load data file
+
+# Load data file
 d <- read.csv("data/data_gamms_baseline-doublecounted.csv",
               stringsAsFactors = FALSE)
 head(d)
 
+# For the plots below, we set ClozeScore of native speakers arbitrarily to 40
+max(d$ClozeScore, na.rm = TRUE)  # Max score among L2ers (no overlap)
+d[d$Group == "NS", "ClozeScore"] <- 40
+
+
 # Convenience function to process Path/Manner subsets of data for GLMMs:
-prepare_df <- function(df = NULL, verbtype = NULL, trials = NULL) {
+prepare_df <- function(df = NULL, verbtype = NULL, trials = NULL, group = NULL) {
   df <- df %>% 
     filter(VerbType == verbtype & Trial %in% trials) %>%
     select(Subject, Condition = Condition_bin, Group, Trial, cTrial, VideoName,
            ClozeScore, Used)
+  # Select group if argument is passed (not NULL)
+  if (! is.null(group) ) {
+    df <- df %>% filter(Group == group) 
+  } else {
+    df$Group <- factor(df$Group)
+    contrasts(df$Group) <- contr.sum(2)
+    colnames(contrasts(df$Group)) <- "L2_vs_NS"
+  }
+  df$cCloze <- df$ClozeScore - mean(df$ClozeScore)
   df$cTrial <- df$Trial - mean(df$Trial)
   df$Subject <- factor(df$Subject)
   df$Condition <- factor(df$Condition)
   contrasts(df$Condition) <- - contr.sum(2)
   colnames(contrasts(df$Condition)) <- "primed_vs_baseline"
-  df$Group <- factor(df$Group)
-  contrasts(df$Group) <- contr.sum(2)
-  colnames(contrasts(df$Group)) <- "L2_vs_NS"
   df
 }
+
 
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -69,10 +84,12 @@ prepare_df <- function(df = NULL, verbtype = NULL, trials = NULL) {
 # trials of interest for the path comparison to be Trials = 1-13
 d_p <- prepare_df(d, "P_V", 1:13)
 head(d_p)
+tail(d_p)
 
 ## Fit GLMM
-# Failures to converge are commented out below; we simplify by-item random
-# effects and take the first model that converges.
+
+# Failures to converge are commented out below; we progressively simplify
+# by-item random effects and take the first model that converges.
 
 # fm_path1 <- glmer(
 #   Used ~ Condition * Group * cTrial + (1 + cTrial | Subject) + 
@@ -101,18 +118,17 @@ head(d_p)
 #   Model is nearly unidentifiable: very large eigenvalue
 #  - Rescale variables?
 
-# # fit model
+# # FINAL model:
 # fm_path <- glmer(
 #   Used ~ Condition * Group * cTrial + (1 + cTrial | Subject) +
-#     (1 + Group + cTrial | VideoName),
+#     (1 + Condition + Group + cTrial | VideoName),
 #   data = d_p, family = 'binomial',
 #   control = glmerControl(optimizer='bobyqa', optCtrl=list(maxfun=2e5))
 #   )
 # saveRDS(fm_path, file = "analysis/gamms/glmm_path.rds")
 
-# or load if already saved to disk
+# load it if already saved to disk
 fm_path <- readRDS("analysis/gamms/glmm_path.rds")
-
 summary(fm_path)
 
 
@@ -121,7 +137,28 @@ summary(fm_path)
 # Plot path verb model ----------------------------------------------------
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-# sjPlot::plot_model - see
+# Add predictions (in log-odds) to data frame
+d_p$Predicted <- predict(fm_path)
+
+# The wiggly lines probably come from the random effects by items
+d_p %>%
+  group_by(Condition, Group, Trial) %>%
+  summarise(Y = mean(Predicted)) %>%
+  ggplot(aes(x = Trial, y = Y, shape = Condition, colour = Group)) +
+  geom_point() +
+  geom_line(aes(linetype = Condition))
+
+# Another option is to fit smooths on the predictions for each trial
+d_p %>%
+  group_by(Condition, Group, Trial, Subject) %>%
+  summarise(Y = mean(Predicted)) %>%
+  ggplot(aes(x = Trial, y = Y, shape = Condition, colour = Group)) +
+  geom_jitter(height = 0, width = .2, alpha = .2) +
+  geom_smooth(aes(linetype = Condition))
+
+
+## Use sjPlot::plot_model - see documentation:
+
 # https://www.rdocumentation.org/packages/sjPlot/versions/2.6.0/topics/plot_model
 # Plotting marginal effects
 # https://strengejacke.github.io/sjPlot/articles/plot_marginal_effects.html
@@ -132,69 +169,57 @@ summary(fm_path)
 plot_model(fm_path)
 plot_model(fm_path, type = "eff", terms = "cTrial")
 plot_model(fm_path, type = "eff", terms = c("cTrial", "Group"))
-plot_model(fm_path, type = "eff", terms = c("cTrial", "Group", "Condition"))
-plot_model(fm_path, type = "pred", terms = c("cTrial", "Group", "Condition"))  # Why different CIs?
+plot_model(fm_path, type = "eff", terms = c("cTrial", "Condition"))
+# Why different CIs for type = "eff" vs "pred"?
+plot_model(fm_path, type = "eff",  terms = c("cTrial", "Condition", "Group"))
+plot_model(fm_path, type = "pred", terms = c("cTrial", "Condition", "Group"))
+# From link above: "type = 'eff' ... is similar to type = 'pred', however,
+# discrete predictors are held constant at their proportions (not reference 
+# level)." 
+# Now, since all predictors are centred or contrast-coded, it shouldn't make
+# much of a difference?
 
 # random effects
 plot_model(fm_path, type = "re")
 plot_model(fm_path, type = "diag")
 
 
-# Add predictions (in log-odds) to data frame
-d_p$Predicted <- predict(fm_path)
-
-d_p %>%
-  group_by(Condition, Group, Trial) %>%
-  summarise(Y = mean(Predicted)) %>%
-  ggplot(aes(x = Trial, y = Y, shape = Condition, colour = Group)) +
-  geom_point() +
-  geom_line(aes(linetype = Condition))
-
-d_p %>%
-  group_by(Condition, Group, Trial, Subject) %>%
-  summarise(Y = mean(Predicted)) %>%
-  ggplot(aes(x = Trial, y = Y, shape = Condition, colour = Group)) +
-  geom_jitter(height = 0, width = .2, alpha = .2) +
-  # geom_point(alpha = .2) +
-  geom_smooth(aes(linetype = Condition))
-
-
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# Extract by-subject coefficient estimates --------------------------------
+# Extract by-subject coefficient estimates: Path --------------------------
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 # Functions are sourced from separate script
 source("analysis/functions/extract-subject-estimates_glmer_fnc.R")
 
-# Take the data frame and for each subject (see row names) output their 
+# Take the data frame and, for each subject (see row names), output their
 # individual model matrix
 (mm_p <- factors2modelmatrix(d_p)) %>% head
 
-# The by-subject coefficients estimated from the model (which add random effect
-# adjustments / BLUPs to fixed effects) are obtained with the coef() function 
-# (see Ben Bolker's answer:
-# https://stackoverflow.com/questions/26198958/extracting-coefficients-and-their-standard-error-from-lme)
+# We can easily obtain the subject-specific estimates, combining fixefs and
+# ranefs:
 head(coef(fm_path)$Subject)
-# Note that the column names match exactly those in the model matrix, so we can
-# matrix multiply by the transpose of the coefficients
+
+# The following function combines the model estimates (fixef and ranef) into
+# two values per subject: 1) their intercept and 2) their slope for Trial:
+(sbj_p <- get_ppt_estimate(mm_p, fm_path, df = d_p)) %>% head
 
 
+## Plot these estimates
 
-ggplot(sbj_p, aes(x = Intercept, y = cTrial, colour = Group)) +
-  geom_point() + 
+# It's difficult to see if L2ers get more native-like with a landscape plot...
+ggplot(sbj_p, aes(x = Intercept, y = Trial, colour = ClozeScore, shape = Group)) +
+  geom_point(size = 2) + 
   facet_grid(. ~ Condition)
 
-coef(fm_path)$Subject %>% head
-coef(fm_path)$Subject %>% str
-rownames(coef(fm_path)$Subject)
+# ... So let's break it down further
+# long format:
+sbj_pl <- gather(sbj_p, coef, estimate, Intercept : Trial)
 
-sbj_coefs_p <- coef(fm_path)$Subject
-sbj_p <- d_p %>%
-  head(sbj_p)
-
-model.matrix(fm_path) %>% head
-
+ggplot(sbj_pl, aes(x = ClozeScore, y = estimate, colour = Group)) +
+  geom_jitter(alpha = .4, size = 1.5) +
+  facet_grid(coef ~ Condition, scales = "free_y") +
+  geom_smooth(method = "lm")
 
 
 
@@ -206,14 +231,112 @@ model.matrix(fm_path) %>% head
 # trials of interest for the path comparison to be Trials = 10-32
 d_m <- prepare_df(d, "M_V", 10:32)
 
-## Fit GLMM
-# Failures to converge are commented out below; we simplify by-item random
-# effects and take the first model that converges.
 
-# fm_manner1 <- glmer(
-#   Used ~ Condition * Group * cTrial + (1 + cTrial | Subject) + 
+## Fit GLMM
+
+# # Maximal model *does* converge
+# fm_manner <- glmer(
+#   Used ~ Condition * Group * cTrial + (1 + cTrial | Subject) +
 #     (1 + Condition * Group * cTrial | VideoName),
-#   data = d_m, family = 'binomial', 
+#   data = d_m, family = 'binomial',
+#   control = glmerControl(optimizer='bobyqa', optCtrl=list(maxfun=2e5))
+#   )
+# saveRDS(fm_manner, file = "analysis/gamms/glmm_manner.rds")
+
+# or load if already saved to disk
+fm_manner <- readRDS("analysis/gamms/glmm_manner.rds")
+summary(fm_manner)
+
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Plot manner verb model --------------------------------------------------
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# Add predictions (in log-odds) to data frame
+d_m$Predicted <- predict(fm_manner)
+
+# Even more wiggly lines than for path (bc more complex by-item ranef structure?)
+d_m %>%
+  group_by(Condition, Group, Trial) %>%
+  summarise(Y = mean(Predicted)) %>%
+  ggplot(aes(x = Trial, y = Y, shape = Condition, colour = Group)) +
+  geom_point() +
+  geom_line(aes(linetype = Condition))
+
+# Another option is to fit smooths on the predictions by trial
+d_m %>%
+  group_by(Condition, Group, Trial, Subject) %>%
+  summarise(Y = mean(Predicted)) %>%
+  ggplot(aes(x = Trial, y = Y, shape = Condition, colour = Group)) +
+  geom_jitter(height = 0, width = .2, alpha = .2) +
+  geom_smooth(aes(linetype = Condition))
+
+
+## Using sjPlot::plot_model()
+
+# fixed effects
+plot_model(fm_manner)
+plot_model(fm_manner, type = "eff", terms = "cTrial")
+plot_model(fm_manner, type = "eff", terms = c("cTrial", "Group"))
+plot_model(fm_manner, type = "eff", terms = c("cTrial", "Condition"))
+plot_model(fm_manner, type = "eff", terms = c("cTrial", "Condition", "Group"))
+plot_model(fm_manner, type = "pred", terms = c("cTrial", "Condition", "Group"))  # Why different CIs?
+
+# random effects
+plot_model(fm_manner, type = "re")
+plot_model(fm_manner, type = "diag")
+
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Extract by-subject coefficient estimates: Manner ------------------------
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# Take the data frame and for each subject (see row names) output their 
+# individual model matrix
+(mm_m <- factors2modelmatrix(d_m)) %>% head
+
+# We can easily obtain the subject-specific estimates, combining fixefs and
+# ranefs:
+head(coef(fm_manner)$Subject)
+
+# The following function comptues two estimates for each subject: their intercept
+# and their slope for Trial (combining fixef and by-subject ranefs):
+(sbj_m <- get_ppt_estimate(mm_m, fm_manner, df = d_m)) %>% head
+
+
+# It's difficult to see if L2ers get more native-like with a landscape plot...
+ggplot(sbj_m, aes(x = Intercept, y = Trial, colour = ClozeScore, shape = Group)) +
+  geom_point() + 
+  facet_grid(. ~ Condition)
+
+# ... So let's break it down further
+# long format:
+sbj_ml <- gather(sbj_m, coef, estimate, Intercept : Trial)
+
+ggplot(sbj_ml, aes(x = ClozeScore, y = estimate, colour = Group)) +
+  geom_jitter(alpha = .4, size = 1.5) +
+  facet_grid(coef ~ Condition, scales = "free_y") +
+  geom_smooth(method = "lm")
+
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Modelling L2 proficiency (Cloze scores) as fixef ------------------------
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+d_l2_p <- prepare_df(d, "P_V", 1:13, "L2")
+d_l2_m <- prepare_df(d, "M_V", 10:32, "L2")
+
+
+## PATH model
+
+# fm_l2_path1 <- glmer(
+#   Used ~ Condition * cTrial * cCloze +
+#     (1 + Condition * cTrial * cCloze | VideoName) +
+#     (1 + cTrial | Subject),
+#   data = d_l2_p, family = 'binomial',
 #   control = glmerControl(optimizer='bobyqa', optCtrl=list(maxfun=2e5))
 #   )
 # 
@@ -221,36 +344,89 @@ d_m <- prepare_df(d, "M_V", 10:32)
 # 1: In checkConv(attr(opt, "derivs"), opt$par, ctrl = control$checkConv,  :
 #   unable to evaluate scaled gradient
 # 2: In checkConv(attr(opt, "derivs"), opt$par, ctrl = control$checkConv,  :
-#   Model failed to converge: degenerate  Hessian with 1 negative eigenvalues
+#   Model failed to converge: degenerate  Hessian with 2 negative eigenvalues
 
-# fm_manner2 <- glmer(
-#   Used ~ Condition * Group * cTrial + (1 + cTrial | Subject) +
-#     (1 + Group * cTrial | VideoName),
-#   data = d_m, family = 'binomial',
+# # FINAL model:
+# fm_l2_path <- glmer(
+#   Used ~ Condition * cTrial * cCloze +
+#     (1 + Condition + cTrial * cCloze | VideoName) +
+#     (1 + cTrial | Subject),
+#   data = d_l2_p, family = 'binomial',
 #   control = glmerControl(optimizer='bobyqa', optCtrl=list(maxfun=2e5))
-#   )
+# )
+# saveRDS(fm_l2_path, file = "analysis/gamms/glmm_l2_path.rds")
+
+# Load it if already fit
+fm_l2_path <- readRDS("analysis/gamms/glmm_l2_path.rds")
+summary(fm_l2_path)
+
+plot_model(fm_l2_path, type = "eff",
+           terms = c("cTrial", "Condition", "cCloze [-15,-10,-5,0,5,10]"))
+
+
+
+## MANNER model
+
+# fm_l2_manner1 <- glmer(
+#   Used ~ Condition * cTrial * cCloze +
+#     (1 + Condition * cTrial * cCloze | VideoName) +
+#     (1 + cTrial | Subject),
+#   data = d_l2_m, family = 'binomial',
+#   control = glmerControl(optimizer='bobyqa', optCtrl=list(maxfun=2e5))
+# )
+# 
+# Warning messages:
+# 1: In optwrap(optimizer, devfun, start, rho$lower, control = control,  :
+#   convergence code 1 from bobyqa: bobyqa -- maximum number of function evaluations exceeded
+# 2: In checkConv(attr(opt, "derivs"), opt$par, ctrl = control$checkConv,  :
+#   unable to evaluate scaled gradient
+# 3: In checkConv(attr(opt, "derivs"), opt$par, ctrl = control$checkConv,  :
+#   Model failed to converge: degenerate  Hessian with 2 negative eigenvalues
+
+
+# fm_l2_manner2 <- glmer(
+#   Used ~ Condition * cTrial * cCloze +
+#     (1 + Condition + cTrial * cCloze | VideoName) +
+#     (1 + cTrial | Subject),
+#   data = d_l2_m, family = 'binomial',
+#   control = glmerControl(optimizer='bobyqa', optCtrl=list(maxfun=2e5))
+# )
 # 
 # Warning messages:
 # 1: In checkConv(attr(opt, "derivs"), opt$par, ctrl = control$checkConv,  :
-#   Model failed to converge with max|grad| = 0.0700389 (tol = 0.001, component 1)
+#   unable to evaluate scaled gradient
 # 2: In checkConv(attr(opt, "derivs"), opt$par, ctrl = control$checkConv,  :
-#   Model is nearly unidentifiable: very large eigenvalue
-#  - Rescale variables?
+#   Model failed to converge: degenerate  Hessian with 2 negative eigenvalues
 
-# # fit model
-# fm_manner <- glmer(
-#   Used ~ Condition * Group * cTrial + (1 + cTrial | Subject) +
-#     (1 + Group + cTrial | VideoName),
-#   data = d_m, family = 'binomial',
+
+# fm_l2_manner3 <- glmer(
+#   Used ~ Condition * cTrial * cCloze +
+#     (1 + Condition * cTrial + cCloze | VideoName) +
+#     (1 + cTrial | Subject),
+#   data = d_l2_m, family = 'binomial',
 #   control = glmerControl(optimizer='bobyqa', optCtrl=list(maxfun=2e5))
 # )
-# saveRDS(fm_manner, file = "analysis/gamms/glmm_manner.rds")
+# 
+# Warning messages:
+# 1: In checkConv(attr(opt, "derivs"), opt$par, ctrl = control$checkConv,  :
+#   unable to evaluate scaled gradient
+# 2: In checkConv(attr(opt, "derivs"), opt$par, ctrl = control$checkConv,  :
+#   Model failed to converge: degenerate  Hessian with 3 negative eigenvalues
 
-# or load if already saved to disk
-fm_manner <- readRDS("analysis/gamms/glmm_manner.rds")
 
+# # FINAL model:
+# fm_l2_manner <- glmer(
+#   Used ~ Condition * cTrial * cCloze +
+#     (1 + Condition + cTrial + cCloze | VideoName) +
+#     (1 + cTrial | Subject),
+#   data = d_l2_m, family = 'binomial',
+#   control = glmerControl(optimizer='bobyqa', optCtrl=list(maxfun=2e5))
+# )
+# saveRDS(fm_l2_manner, file = "analysis/gamms/glmm_l2_manner.rds")
 
-summary(fm_manner)
+# Load it if already fit
+fm_l2_manner <- readRDS("analysis/gamms/glmm_l2_manner.rds")
+summary(fm_l2_manner)
 
-plot_model(fm_manner)
-
+plot_model(fm_l2_manner, type = "eff", 
+           terms = c("cTrial", "Condition", "cCloze [-15,-10,-5,0,5,10]"))
--- analysis/functions/extract-subject-estimates_glmer_fnc.R
+++ analysis/functions/extract-subject-estimates_glmer_fnc.R
@@ -6,7 +6,9 @@
 library(dplyr)
 
 
+
 # factors2modelmatrix -----------------------------------------------------
+
 
 # Given participant factor levels, output the right model matrix to multiply
 # their model coefficients by:
@@ -23,3 +25,61 @@ factors2modelmatrix <- function(
   mm
 }
 
+
+
+# Get intercept and trial slope per subject -------------------------------
+
+
+# For each subject, we want to obtain two estimates:
+# 1) Subject-specific Intercept: adding fixef and ranefs specific to that subject
+# 2) Subject-specific Trial effect: again adding fixef and ranefs
+
+# Function to do the basic computation per subject:
+# Takes two vectors, one represents the subject-specific predictors, the other
+# the subject-specific estimates. It outputs a 2-valued vector: Subject-specific
+# Intercept and Subject-specific Trial effect
+compute_ppt_estimate <- function (
+  sbj_pred = NULL,
+  sbj_coef = NULL
+  ) {
+  # first divide sbj_coef into the intercept part and the cTrial slope:
+  b <- diag(sbj_coef) %*% matrix(c(1,1,1,0,1,0,0,0,   # coefficients for intercept
+                                   0,0,0,1,0,1,1,1),  # coefficients for cTrial slope
+                                 ncol = 2)
+  # Now predictor vector matrix-multiplies b to obtain the 2 estimates
+  sbj_pred %*% b
+}
+
+
+# wrapper function to do the same thing for all subjects
+# NB: The by-subject coefficients estimated from the model (which add random
+# effect adjustments / BLUPs to fixed effects) are obtained with the coef() 
+# function (see Ben Bolker's answer:
+# https://stackoverflow.com/questions/26198958/extracting-coefficients-and-their-standard-error-from-lme)
+get_ppt_estimate <- function (
+  mm_sbj = NULL,  # output of factors2modelmatrix() 
+  fm = NULL,  # we'll use output of coef(fm)$Subject
+  df = NULL  # dataframe used to fit model to get subject info in last step
+  ) {
+  # Through matrix multiplication, we'll obtain the two estimates above:
+  coefs <- coef(fm)$Subject
+
+  # Sanity check: verify same subjects in both arrays
+  if ( (sum(! row.names(mm_sbj) %in%  row.names(coefs) )  != 0 ) |
+       (sum(! row.names(coefs)  %in%  row.names(mm_sbj) ) != 0 )) {
+    stop ("Subjects are not the same!")
+  } else {  # same row order in both (by subject)
+    mm_sbj <- mm_sbj[rownames(coefs), ]
+  }
+  
+  # Compute the 2 estimates for each subject using compute_ppt_estimate()
+  ppt_estimates <- matrix(NA, nrow = nrow(mm_sbj), ncol = 2)
+  for (S in 1 : nrow(mm_sbj)) {
+    ppt_estimates[S, ] <- compute_ppt_estimate(mm_sbj[S, ], coefs[S, ])
+  }
+  out <- data.frame(ppt_estimates, Subject = rownames(mm_sbj))
+  names(out)[1:2] <- c("Intercept", "Trial")
+  # Combine with subject information
+  out <- left_join(out, df %>% select(Subject, Condition, Group, ClozeScore) %>% unique)
+  out %>% select(Subject:ClozeScore, Intercept, Trial)
+}
